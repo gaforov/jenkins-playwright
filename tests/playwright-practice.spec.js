e43927c2c662dab0cabb70d90c454829a73806b0
Said Gaforov <sgaforov@gmail.com>
2025-01-08 12:18:04 -0800

    fixing Youtube Search test that fails, now fixed.

--- tests/playwright-practice.spec.js
+++ tests/playwright-practice.spec.js
@@ -2,17 +2,12 @@ import test from "@playwright/test";
 
 test('Youtube Search', async ({page}) => {
     await page.goto('https://www.youtube.com/')
-
-    //await page.waitForTimeout(2000);
-    const searchInput = page.locator('input#search');
-    const searchButton = page.locator('#search-icon-legacy');
+    const searchInput = page.getByRole('combobox', {name: 'Search'});
+    // const searchButton = page.getByRole('button', {name: 'Search'});  // this not unique locator, fails. 
+    const searchButton = page.getByRole('button', {name: 'Search', exact: true});   // Ensures that only elements with an exact match for name: 'Search' are selected.
+    // const searchButton = page.locator("(//button[@title='Search'])[1]");  // this xpath is unique too, works
     
     await searchInput.fill('Playwright Automation');
-    //await page.waitForTimeout(2000);
-    await searchButton.click();
-    await searchInput.press('Enter')
+    await searchButton.click();   
+    // await searchInput.press('Enter');   // press works too, mimicks user pressing enter on the keyboard after the search
 });
-
-/* 
-<input id="search" autocapitalize="none" autocomplete="off" autocorrect="off" name="search_query" tabindex="0" type="text" spellcheck="false" placeholder="Search" aria-label="Search" role="combobox" aria-haspopup="false" aria-autocomplete="list" dir="ltr" class="ytd-searchbox" style="outline: none;">
-*/
